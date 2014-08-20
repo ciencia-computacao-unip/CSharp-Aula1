dcbd24d9265232afca5ab405db9d1e55f76ed31b
Victor Perin <victor_perin@nerdhitech.com.br>
2014-08-19 22:46:39 -0300

    Adicionando exercício 1

--- Program.cs
+++ Program.cs
@@ -15,6 +15,16 @@ namespace ConsoleApplication1
             Console.Title = "C#, Aula1 - Exercícios";
             //fim da viadagem
 
+            //inicio exercício 1
+            Console.WriteLine("1) Faça um programa que exiba na tela o resultado da soma dos números 55 e 60.\n");
+            int primeiro_numero, segundo_numero, soma;
+            primeiro_numero = 55;
+            segundo_numero = 60;
+            Console.WriteLine("O Primeiro número é "+primeiro_numero);
+            Console.WriteLine("O Segundo número é " + segundo_numero);
+            soma = primeiro_numero + segundo_numero;
+            Console.WriteLine("A soma dos dois números é " + soma);
+            //fim exercício 1
 
 
 
